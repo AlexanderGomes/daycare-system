bc9f4bf352cfc6d8aa9d26fe4ff8bb44fecac2db
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2022-12-22 21:48:50 -0800

    done

--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -4,7 +4,7 @@ import Moment from "react-moment";
 
 const List = ({ history, paid, unpaid }) => {
   return (
-    <div className="list__all">
+    <div>
       {paid && history.isPaid === true ? (
         <div className="list__main">
           <div className="list__info">
@@ -32,10 +32,9 @@ const List = ({ history, paid, unpaid }) => {
         ""
       )}
 
-      {unpaid  && history?.isPaid === false ? (
+      {unpaid && history?.isPaid === false ? (
         <div className="list__main">
           <div className="list__info">
-          
             <div className="list__from">
               <span className="from">From: </span>
               <Moment format="DD/MM/YYYY" className="list__dates">
@@ -55,13 +54,10 @@ const List = ({ history, paid, unpaid }) => {
               <span className="to">Price: ${history?.price} </span>
             </div>
           </div>
-        
         </div>
       ) : (
         ""
       )}
-
-  
     </div>
   );
 };
